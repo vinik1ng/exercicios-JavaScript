cbdc1e4930081b8965feeebd20f6b55dabfdc0c9
vinik1ng <102757784+vinik1ng@users.noreply.github.com>
2022-05-04 16:44:49 -0300

    Add files via upload

---
+++ exercicio.js
@@ -0,0 +1,36 @@
+//questões EAD
+
+//1
+console.log (10+20*30);
+console.log (4**2/30);
+console.log ((9**4)*6-1);
+
+//2
+console.log (10%3*10**1+1-10*4/2);
+
+
+//3
+const nome = "Vinicius";
+console.log(nome)
+
+//4
+
+const x = 3
+const y = 5
+
+console.log(2*x*3*y)
+
+//5
+
+const a = 10
+const b = 40
+const c = 24
+
+console.log(a+b+c)
+
+//6
+
+const salario = 1800
+const aumento = 2
+
+console.log(salario*aumento)
